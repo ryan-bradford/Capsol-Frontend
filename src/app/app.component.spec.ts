7585b68259685a8f33ffcd5c0bae38030f6138dd
Ryan Bradford <ryanbradford333@gmail.com>
2019-11-03 11:02:37 -0500

    Renamed repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'solar-frontend'`, () => {
+  it(`should have as title 'Capsol'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('solar-frontend');
+    expect(app.title).toEqual('Capsol');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('solar-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Capsol app is running!');
   });
 });
